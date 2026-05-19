f513625b23690c6c8ff0df683a5af84fb4b346a5
André Meira <andpmeira@gmail.com>
2026-05-19 18:59:07 +0100

    Update graf_res.py

--- graf_res.py
+++ graf_res.py
@@ -190,7 +190,7 @@ def get_logo_html(filename="logo.png"):
     if os.path.exists(filename):
         with open(filename, 'rb') as f:
             data = base64.b64encode(f.read()).decode()
-        return f"<img src='data:image/png;base64,{data}' width='300' style='margin-bottom: 5px;'>"
+        return f"<img src='data:image/png;base64,{data}' width='300' style='margin-bottom: 0px;'>"
     return "<div style='font-size: 50px;'>🫀</div>"
 
 st.sidebar.markdown(f"""
